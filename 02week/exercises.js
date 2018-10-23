9161bfc5b5c0b5d2510cc1daef23d9547a770f39
katiemacaulay <katiemmacaulay@gmail.com>
2018-10-23 18:27:35 -0500

    more

---
+++ 02week/exercises.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const cars = ['Ford', 'Nissan', 'Mazda', 'Jeep'];
+console.log(cars.length);
+
+const moreCars = ['Tesla', 'BMW', 'Audi', 'Honda'];
+
+const totalCars = cars.concat(moreCars);
+console.log(totalCars.indexOf('Honda'));
+
