43bb82b728d83631457934c063ff878f6de5125b
katiemacaulay <katiemmacaulay@gmail.com>
2018-10-25 19:06:49 -0500

    start

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const printListOfOlympians = (arr) =>{
+    arr.forEach(console.log(arr))
+}
+
+arrh = [ 'Spencer', 'Super Mario', 'Jose', 'Cat Power', 'Lady Heart']
+
+printListOfOlympians(arrh)
