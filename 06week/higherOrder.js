a2f44161f957ea3ad015ddfd925b38f0bcec638b
katiemacaulay <katiemmacaulay@gmail.com>
2018-11-18 18:25:46 -0600

    changes to be commited

--- 06week/higherOrder.js
+++ 06week/higherOrder.js
@@ -2,37 +2,45 @@
 
 const assert = require('assert');
 
-function forEach(arr, callback) {
-  // Your code here
+function myForEach(arr, callback) {
+  for(let i = 0 ; i< arr.length ; i++ ){
+    callback(arr[i])
+  }
 }
 
-function map(arr, callback) {
-  newArr = [];
+function map(arr, mapped) {
+  const newArr = [];
   for(let i = 0 ; i< arr.length ; i++ ){
-    const formattedItem = callback(arr[i])
+    const formattedItem = mapped(arr[i])
     newArr.push(formattedItem)
   }
   return newArr
 }
 
 function filter(arr, callback) {
-  // Your code here
+  const newArr = [];
+  for(let i = 0 ; i < arr.length ; i++){
+    if(callback(arr[i])){
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr
 }
 
 function some(arr, callback) {
   // Your code here
 }
 
-function every(arr, callback) {
-  // Your code here
-}
+// function every(arr, callback) {
+//   // Your code here
+// }
 
 if (typeof describe === 'function') {
 
-  describe('#forEach()', () => {
+  describe('#myForEach()', () => {
     it('should call the callback the array.length number of times', () => {
       let count = 0;
-      forEach([1, 2, 3], () => {
+      myForEach([1, 2, 3], () => {
         count++;
       });
       assert.equal(count, 3);
@@ -61,45 +69,45 @@ if (typeof describe === 'function') {
     });
   });
 
-  describe('#some()', () => {
-    let count = 0;
-    const somed = some([1, 2, 3, 4], (num) => {
-      count++;
-      return num % 2 === 0;
-    });
-    it('should return true if at least one item passes the predicate test', () => {
-      assert.equal(somed, true);
-    });
-    it('should stop at the first item that passes the predicate test', () => {
-      assert.equal(count, 2);
-    });
-    it('should return false if no items pass the predicate test', () => {
-      const somed = some([1, 3, 5], (num) => {
-        return num % 2 === 0;
-      });
-      assert.equal(somed, false);
-    });
-  });
+  // describe('#some()', () => {
+  //   let count = 0;
+  //   const somed = some([1, 2, 3, 4], (num) => {
+  //     count++;
+  //     return num % 2 === 0;
+  //   });
+  //   it('should return true if at least one item passes the predicate test', () => {
+  //     assert.equal(somed, true);
+  //   });
+  //   it('should stop at the first item that passes the predicate test', () => {
+  //     assert.equal(count, 2);
+  //   });
+  //   it('should return false if no items pass the predicate test', () => {
+  //     const somed = some([1, 3, 5], (num) => {
+  //       return num % 2 === 0;
+  //     });
+  //     assert.equal(somed, false);
+  //   });
+  // });
 
-  describe('#every()', () => {
-    it('should return true if at all passes the predicate test', () => {
-      const everied = every([2, 4, 6], (num) => {
-        return num % 2 === 0;
-      });
-      assert.equal(everied, true);
-    });
-    let count = 0;
-    const everied = every([2, 3, 4, 5], (num) => {
-      count++;
-      return num % 2 === 0;
-    });
-    it('should return false if any item fails the predicate test', () => {
-      assert.equal(everied, false);
-    });
-    it('should stop at the first item that fails the predicate test', () => {
-      assert.equal(count, 2);
-    });
-  });
+  // describe('#every()', () => {
+  //   it('should return true if at all passes the predicate test', () => {
+  //     const everied = every([2, 4, 6], (num) => {
+  //       return num % 2 === 0;
+  //     });
+  //     assert.equal(everied, true);
+  //   });
+  //   let count = 0;
+  //   const everied = every([2, 3, 4, 5], (num) => {
+  //     count++;
+  //     return num % 2 === 0;
+  //   });
+  //   it('should return false if any item fails the predicate test', () => {
+  //     assert.equal(everied, false);
+  //   });
+  //   it('should stop at the first item that fails the predicate test', () => {
+  //     assert.equal(count, 2);
+  //   });
+  // });
 
 } else {
 
